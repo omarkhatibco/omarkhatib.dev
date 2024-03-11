520948324d681cda9bd89ffc0aaafa8806140883
Omar Khatib <omar.khatib@tourlane.com>
2024-03-11 01:43:28 +0100

    fix: cleaning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      {/* <link rel='icon' href='/favicon.ico' /> */}
-
       <body className={inter.className}>{children}</body>
     </html>
   );

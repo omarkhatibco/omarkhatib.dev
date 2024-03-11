0bb73f634845cd499bcf6b26a8b52f174fa7a61b
Omar Khatib <omar.khatib@tourlane.com>
2024-03-11 01:42:15 +0100

    fix: fix favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
     'Omar Khatib | Senior Software Engineer | Next.js • React.js • Node.js • TypeScript • PostgreSQL',
   description:
     'I am a Senior Software Engineer with over 10 years of web development experience, My goal is to deliver high-quality, innovative solutions that solve real-world problems and enhance customer satisfaction. I have a strong background in React, Typescript, GraphQL, Next.js, and Node.js, and am always eager to learn new technologies and best practices.',
+  icons: '/favicon.ico',
 };
 
 export default function RootLayout({
@@ -18,6 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      {/* <link rel='icon' href='/favicon.ico' /> */}
+
       <body className={inter.className}>{children}</body>
     </html>
   );

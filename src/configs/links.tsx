9b249957c27e2a3b0c1109daebf5389fdbbc881b
Omar Khatib <omar@omarkhatib.co>
2024-11-16 04:29:55 +0100

    remove linkedin

--- src/configs/links.tsx
+++ src/configs/links.tsx
@@ -1,19 +1,19 @@
-import { Github, LinkedIn, Mail, Twitter, Xing } from '@/svgs';
+import { Github, LinkedIn, Mail, Twitter, Xing } from "@/svgs";
 
 export const links = [
   {
-    title: 'Github',
-    href: 'https://github.com/omarkhatibco',
+    title: "Github",
+    href: "https://github.com/omarkhatibco",
     icon: <Github />,
   },
+  // {
+  //   title: 'LinkedIn',
+  //   href: 'https://www.linkedin.com/in/omarkhatibco/',
+  //   icon: <LinkedIn />,
+  // },
   {
-    title: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/omarkhatibco/',
-    icon: <LinkedIn />,
-  },
-  {
-    title: 'Twitter',
-    href: 'https://twitter.com/omarkhatibco',
+    title: "Twitter",
+    href: "https://twitter.com/omarkhatibco",
     icon: <Twitter />,
-  }
+  },
 ];
